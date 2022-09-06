65c3f8109d0bb87fed42d97b51e0befa452990a9
Leo Ho <leo.ho@mindera.com>
2022-09-06 18:49:55 +0100

    routes created, active links explained.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,120 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  Link,
+  NavLink,
+} from "react-router-dom";
+
+// function Home() {
+//   return (
+//     <React.Fragment>
+//       <div>
+//         <h1>Hello World!</h1>
+//         <p>This the the Home Page</p>
+//       </div>
+//     </React.Fragment>
+//   );
+// }
+
+// function About() {
+//   return (
+//     <React.Fragment>
+//       <div>
+//         <h1>About Me!</h1>
+//         <p>This the the About Page</p>
+//       </div>
+//     </React.Fragment>
+//   );
+// }
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <nav style={{ padding: "3px", borderBottom: "solid 1px" }}>
+//         <NavLink
+//           to="/"
+//           style={({ isActive }) =>
+//             isActive ? { backgroundColor: "green" } : undefined
+//           }
+//         >
+//           Home
+//         </NavLink>{" "}
+//         |{" "}
+//         <NavLink
+//           to="/about"
+//           style={({ isActive }) =>
+//             isActive ? { backgroundColor: "green" } : undefined
+//           }
+//         >
+//           About
+//         </NavLink>
+//       </nav>
+
+//       <Routes>
+//         <Route path="/about" element={<About />} />
+//         <Route path="/" element={<Home />} />
+//       </Routes>
+//     </BrowserRouter>
+//   );
+// }
+
+function Home() {
+  return <h1>Home</h1>;
+}
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <Outlet />
+    </div>
+  );
+}
+
+function About1() {
+  return <h1>Part 1</h1>;
+}
+
+function About2() {
+  return <h1>Part 2</h1>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <BrowserRouter>
+      <nav>
+        <NavLink
+          to="/home"
+          style={({ isActive }) => {
+            if (isActive) return { backgroundColor: "lightgreen" };
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          Home
+        </NavLink>{" "}
+        |{" "}
+        <NavLink
+          to="/about"
+          style={(obj) => {
+            if (obj.isActive) return { backgroundColor: "lightgreen" };
+          }}
+        >
+          About
+        </NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/home" element={<Home />}></Route>
+        <Route path="/about" element={<About />}>
+          <Route path="part1" element={<About1 />}></Route>
+          <Route path="part2" element={<About2 />}></Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
